6f7d5164ebfee79b6dded2a9025049b6a34c2747
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2021-06-07 17:46:45 -0400

    ad support to pull in root 1 and root 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App2">
+      <header className="App2-header">
         <h1>React App 2</h1>
         <p>React App 2 is the main app being loaded, React App1 is pulled in through index.html</p>
       </header>
