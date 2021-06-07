e14326396b67d05e2c830bbbe5f3bdbf4d3c0822
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2021-06-07 17:43:54 -0400

    commit and add loaded assets

---
+++ public/loadApp1.js
@@ -0,0 +1,57 @@
+console.log("Hello world!");
+
+const fetchAssets = async () => {
+  try {
+    console.log("fetchAssets executed");
+
+    const request = await (
+      await fetch(`https://csb-1mbs6.netlify.app/asset-manifest.json`)
+    ).json();
+
+    console.log(request, "REQUEST");
+    importFilesFromArray(request.entrypoints, "https://csb-1mbs6.netlify.app/");
+  } catch (error) {
+    console.error(error, "error");
+    console.error("Error fetching app 1 assets");
+  }
+};
+
+// Takes the provided array, parses it and loads all of the
+// JS and CSS files based on their file extension.
+function importFilesFromArray(files, rootURL) {
+  console.log(files, "FILES");
+  for (var i = 0; i < files.length; i++) {
+    if (files[i].endsWith(".js")) {
+      importJSFile(rootURL + files[i]);
+    } else if (files[i].endsWith(".css")) {
+      importCSSFile(rootURL + files[i]);
+    }
+  }
+}
+
+// Imports a single JS file.
+function importJSFile(url) {
+  console.log(url, "url");
+  var jsElement = document.createElement("script");
+  jsElement.src = url;
+  jsElement.type = "text/javascript";
+
+  var head = document.head;
+  head.insertBefore(jsElement, head.childNodes[0]);
+}
+
+// Imports a single CSS file.
+function importCSSFile(url) {
+  var cssElement = document.createElement("link");
+  cssElement.href = url;
+  cssElement.rel = "stylesheet";
+  cssElement.type = "text/css";
+
+  var head = document.head;
+  head.insertBefore(cssElement, head.childNodes[0]);
+}
+
+
+window.onload = function () {
+  fetchAssets();
+};
