18ae526681c01d214fa4ea6fda3dfeef0e3587fb
Vincent Tang <10290348+vincentntang@users.noreply.github.com>
2021-06-07 17:41:48 -0400

    commit to master push

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React App 2</h1>
+        <p>React App 2 is the main app being loaded, React App1 is pulled in through index.html</p>
       </header>
     </div>
   );
